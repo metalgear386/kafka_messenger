0eba63bcd26a9b9b7cff98185c0cee9f236fb76b
Jeremy Gillespie <jeremycharlesgillespie@gmail.com>
2017-12-06 22:48:00 -0600

    Added sender.py, for a more focused on sending effort.

---
+++ sender.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""This program is used to send messages via command line input from the user.
+Author: Jeremy Gillespie
+"""
+import logging
+import os
+import sys
+import message_sender as ms
+
+def my_handler(exc_type, value, traceback_call):
+    """This method defines how exceptions will be written to log if they are
+    not caught exceptions.
+    """
+    print(str(exc_type))
+    print(traceback_call)
+    LOGGER.exception("Uncaught exception:" + str(value) + str(exc_type))
+
+def start_logging(file_name):
+    """Starts logging on the machine, using the file name as the preface
+    to the log's name. We also check to see if the correct folder exists and
+    output error messages if it doesnt exist or doesnt have permission correctly.
+    """
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    handler = logging.FileHandler(file_name + ".log", mode='a')
+    handler.setLevel(logging.INFO)
+    formatting = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatting)
+    logger.addHandler(handler)
+    return logger
+
+BASE = os.path.basename(__file__)
+LOGGER = start_logging(BASE)
+#sys.excepthook = my_handler
+LOGGER = start_logging(BASE)
+SENDER = ms.LOGSENDER()
+LOGSRC = "gman"
+#EXAMPLE_MESSAGES = ["1", "2", "3", "4"]
+MESSAGE = [str(input())]
+
+SENDER.send_list_of_logs(SENDER, LOGSRC, MESSAGE)
