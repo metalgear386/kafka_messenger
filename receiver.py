32da3bc6629c910176001f389906ba2b21c088d7
Jeremy Gillespie <jeremycharlesgillespie@gmail.com>
2017-12-10 03:01:40 -0600

    PEP8, made a new method that pulls all the messages in the history of the queue...or as long as we have log retention (168 hours?)

--- receiver.py
+++ receiver.py
@@ -1,20 +1,38 @@
 #!/usr/bin/python3
+"""This file is imported and used to pull messages from kafka.
+"""
 import json
 from kafka import KafkaConsumer
 
 class RECEIVER(object):
+    """This is a class that gets created per user session to receive messages
+    from kafka.
+    """
     def __init__(self):
+        """This creates a new consumer and starts comsuming from the latest
+        message for a topic
+        """
         self.consumer = KafkaConsumer()#, auto_offset_reset="earliest")
         self.consumer.subscribe(['Test'])
         #print("Receiver is connected!")
 
     def check_for_msgs(self):
+        """This method pulls each message from the queue, parses it and returns
+        JSON in the form of user: message.
+        """
         for msg in self.consumer:
-                #print(msg)
-                message = json.loads(msg.value.decode())
-                return ": ".join([message["user"], message["msg"]])  
+            #print(msg)
+            message = json.loads(msg.value.decode())
+            return ": ".join([message["user"], message["msg"]])
+
+    def get_all_msgs(self):
+        """This method gets all messages in the history for a queue. Allows a user
+        to see the past messages that happened while they weren't logged in.
+        """
+        self.consumer = KafkaConsumer(auto_offset_reset="earliest")
+        self.consumer.subscribe(['Test'])
 
 if __name__ == "__main__":
-    rc = RECEIVER()
+    RC = RECEIVER()
     while True:
-        print(rc.check_for_msgs())
+        print(RC.check_for_msgs())
